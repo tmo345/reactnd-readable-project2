4196c6d06bfc34501fac179c91e309610c9129c1
Timothy Moore <moore.tim@outlook.com>
2017-09-05 15:36:48 -0400

    refactor: Rename action types and add type annotations

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 // @flow
+
 export const UP_VOTE_POST = 'UP_VOTE_POST';
 export const DOWN_VOTE_POST = 'DOWN_VOTE_POST';
 
@@ -13,36 +14,94 @@ export const ADD_COMMENT = 'ADD_COMMENT';
 export const EDIT_COMMENT = 'EDIT_COMMENT';
 export const DELETE_COMMENT = 'DELETE_COMMENT';
 
+export type UpVotePost = { type: 'UP_VOTE_POST', id: string }
+export type DownVotePost = { type: 'DOWN_VOTE_POST', id: string }
+export type UpVoteComment = { type: 'UP_VOTE_COMMENT', id: string }
+export type DownVoteComment = { type: 'DOWN_VOTE_COMMENT', id: string }
 
-export function upVotePost({ postId }) {
+export type AddPost = {
+  type: typeof ADD_POST,
+  id: string,
+  timestamp: number,
+  title: string,
+  body: string,
+  author: string,
+  category: string
+}
+
+export type EditPost = {
+  type: typeof EDIT_POST,
+  id: string,
+  title: string,
+  body: string
+}
+
+export type DeletePost = {
+  type: typeof DELETE_POST,
+  id: string
+}
+
+export type AddComment = {
+  type: typeof ADD_COMMENT,
+  id: string,
+  timestamp: number,
+  body: string,
+  author: string,
+  parentId: string
+}
+
+export type EditComment = {
+  type: typeof EDIT_COMMENT,
+  timestamp: number,
+  body: string
+}
+
+export type DeleteComment = {
+  type: typeof DELETE_COMMENT,
+  id: string
+}
+
+export type Action =
+  | UpVotePost
+  | DownVotePost
+  | UpVoteComment
+  | DownVoteComment
+  | AddPost
+  | EditPost
+  | DeletePost
+  | AddComment
+  | EditComment
+  | DeleteComment;
+
+export function upVotePost(id: string): UpVotePost {
   return {
     type: UP_VOTE_POST,
-    postId
+    id
   }
 }
 
-export function downVotePost({ postId }) {
+export function downVotePost(id: string): DownVotePost {
   return {
     type: DOWN_VOTE_POST,
-    postId
+    id
   }
 }
 
-export function upVoteComment({ commentId }) {
+export function upVoteComment(id: string): UpVoteComment {
   return {
     type: UP_VOTE_COMMENT,
-    commentId
+    id
   }
 }
 
-export function downVoteComment({ commentId }) {
+export function downVoteComment(id: string): DownVoteComment {
   return {
     type: DOWN_VOTE_COMMENT,
-    commentId
+    id
   }
 }
 
-export function addPost({ id, timestamp, title, body, author, category }) {
+export function addPost(id: string, timestamp: number, title: string, body: string, author: string, category: string): AddPost {
   return {
     type: ADD_POST,
     id,
@@ -54,23 +113,23 @@ export function addPost({ id, timestamp, title, body, author, category }) {
   }
 }
 
-export function editPost({ postId, title, body }) {
+export function editPost(id: string, title: string, body: string): EditPost {
   return {
     type: EDIT_POST,
-    postId,
+    id,
     title,
     body
   }
 }
 
-export function deletePost({ postId }) {
+export function deletePost(id: string): DeletePost {
   return {
     type: DELETE_POST,
-    postId
+    id
   }
 }
 
-export function addComment( { id, timestamp, body, author, parentId } ) {
+export function addComment(id: string, timestamp: number, body: string, author: string, parentId: string ): AddComment {
   return {
     type: ADD_COMMENT,
     id,
@@ -81,7 +140,7 @@ export function addComment( { id, timestamp, body, author, parentId } ) {
   }
 }
 
-export function editComment( { commentId, timestamp, body } ) {
+export function editComment(id: string, timestamp: number, body: string ): EditComment {
   return {
     type: EDIT_COMMENT,
     timestamp,
@@ -89,9 +148,9 @@ export function editComment( { commentId, timestamp, body } ) {
   }
 }
 
-export function deleteComment( { commentId } ) {
+export function deleteComment(id: string): DeleteComment {
   return {
     type: DELETE_COMMENT,
-    commentId
+    id
   }
 }
