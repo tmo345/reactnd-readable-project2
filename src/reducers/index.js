c6f369baf7f123b85cf3b718fc33db29452a4670
Timothy Moore <moore.tim@outlook.com>
2017-09-06 18:03:48 -0400

    feat: Define initialPostState object and posts reducer
    posts reducer is responsible for accepting a PostState and a PostAction
    (AddPost | EditPost | DeletePost) and returning a PostState.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -42,21 +42,72 @@ type PostState = {|
   allIds: Array<string>
 |}
 
-const initialState = {
-  category: {},
-  posts: {
-    "abcdefg": {
-      id: "abcdefg",
-      timestamp: 42,
-      title: 'title',
-      body: 'body',
-      author: 'author',
-      category: 'category',
-      votedScore: 2,
-      deleted: false
-    }
-  },
-  comments: {}
+const initalPostState : PostState = {
+  byId: {},
+  allIds: []
+};
+
+// posts : (PostState, PostAction) -> PostState
+const posts = (state : PostState = initalPostState, action: PostAction): PostState => {
+  switch (action.type) {
+    case ADD_POST:
+      { // to block scope the const declarations to this case
+        const { id, timestamp, title, body, author, category, votedScore } = action.postInfo;
+        return {
+          byId: {
+            ...state.byId,
+            [id]: {
+              id,
+              timestamp,
+              title,
+              body,
+              author,
+              category,
+              votedScore
+            }
+          },
+          allIds: [
+            ...state.allIds,
+            id
+          ]
+        }
+      }
+    case EDIT_POST:
+      {
+        const { id, title, body } = action.postInfo;
+        return {
+          byId: {
+            ...state.byId,
+            [action.postInfo.id]: {
+              ...state.byId[id],
+              title,
+              body
+            }
+          },
+          allIds: [...state.allIds]
+        }
+      }
+    case DELETE_POST:
+      {
+        const { id } = action.postInfo;
+        // Filter allIds to include all but the id of post to be deleted
+        const remainingIds = state.allIds.filter((remainingId) => remainingId !== id);
+
+        // Reduce remainingIds array to build new byId object using the remainingIds as
+        // properties and the objects with corresponding ids in state.
+        const remainingPosts = remainingIds.reduce((byId, currentId) => {
+            byId[currentId] = state['byId'][currentId];
+            return byId;
+          }, {});
+
+        return {
+          byId: remainingPosts,
+          allIds: remainingIds
+        }
+      }
+    default:
+      return state;
+  }
 }
 
 export default function reducer(state: State = initialState, action: Action): State {
