926d8446d70eef6e91d501716133985a6bdc1955
Timothy Moore <moore.tim@outlook.com>
2017-09-07 15:27:25 -0400

    feat: Define string constants for actions

--- src/immutableapp.js
+++ src/immutableapp.js
@@ -37,3 +37,17 @@ const initalStateJS: State = {
 const initialState: Map<string,*> = fromJS(initalStateJS);
 
 
+export const UP_VOTE_POST = 'UP_VOTE_POST';
+export const DOWN_VOTE_POST = 'DOWN_VOTE_POST';
+
+export const ADD_POST = 'ADD_POST';
+export const EDIT_POST = 'EDIT_POST';
+export const DELETE_POST = 'DELETE_POST';
+
+export const UP_VOTE_COMMENT = 'UP_VOTE_COMMENT';
+export const DOWN_VOTE_COMMENT = 'DOWN_VOTE_COMMENT';
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+
