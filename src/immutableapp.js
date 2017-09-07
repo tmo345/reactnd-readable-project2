9e771f8104ac42c24ce2670c7ce5a8414d4ef500
Timothy Moore <moore.tim@outlook.com>
2017-09-07 15:24:52 -0400

    feat: Define State type, which is the shape of redux store
    Define for a javascript object instead of immutable Map since
    there is not a way to define a flow type for a Map like this.

--- src/immutableapp.js
+++ src/immutableapp.js
@@ -3,3 +3,26 @@ import { fromJS, Map } from 'immutable';
 
 const uuidv4 = require('uuid/v4');
 
+type State = {
+  posts: {
+      [id: string]: {
+        id: string,
+        timestamp: number,
+        title: string,
+        body: string,
+        author: string,
+        category: string,
+        votedScore: number
+      }
+  },
+  comments: {
+      [id: string]: {
+        id: string,
+        parentId: string,
+        timestamp: string,
+        body: string,
+        author: string,
+        votedScore: number
+      }
+  }
+}
