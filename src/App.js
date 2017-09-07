b8d3dbef27286f49e77fc61ba35fecea20d0f2f0
Timothy Moore <moore.tim@outlook.com>
2017-09-07 13:35:45 -0400

    feat: Use connect to dispatch addPosts for testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { addPost } from './actions'
+import { addPost } from './actions';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
       <div className="App"
-        onClick={() => this.props.store.dispatch(addPost({ title: 'hi', body: 'hi body', author: 'author', category: 'category'}))}>
+        onClick={() => this.props.onDivClick({title: 'title', body: 'body', author: 'author', category: 'category'})}>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -19,5 +20,14 @@ class App extends Component {
     );
   }
 }
+const mapStateToProps = () => {
+  return {}
+}
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onDivClick: (data) => dispatch(addPost(data))  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
