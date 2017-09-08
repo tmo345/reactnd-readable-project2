1242aedd741d2f9d32e22d2dfe60c619b84071fa
Timothy Moore <moore.tim@outlook.com>
2017-09-07 22:20:21 -0400

    style: Clean up spacing and formatting

--- src/immutableapp.js
+++ src/immutableapp.js
@@ -5,33 +5,31 @@ const uuidv4 = require('uuid/v4');
 
 type State = {
   posts: {
-      [id: string]: {
-        id: string,
-        timestamp: number,
-        title: string,
-        body: string,
-        author: string,
-        category: string,
-        votedScore: number
-      }
+    [id: string]: {
+      id: string,
+      timestamp: number,
+      title: string,
+      body: string,
+      author: string,
+      category: string,
+      votedScore: number
+    }
   },
   comments: {
-      [id: string]: {
-        id: string,
-        parentId: string,
-        timestamp: string,
-        body: string,
-        author: string,
-        votedScore: number
-      }
+    [id: string]: {
+      id: string,
+      parentId: string,
+      timestamp: string,
+      body: string,
+      author: string,
+      votedScore: number
+    }
   }
 }
 
 const initalStateJS: State = {
-  posts: {
-  },
-  comments: {
-  }
+  posts: {},
+  comments: {}
 };
 
 const initialState: Map<string,*> = fromJS(initalStateJS);
