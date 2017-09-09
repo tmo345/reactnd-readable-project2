5534610ef4de9076190964beadb0f55cc2a84052
Timothy Moore <moore.tim@outlook.com>
2017-09-09 15:19:43 -0400

    refactor: Change Auth token to string constant

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,8 @@
+// @flow
+
 const api = "http://localhost:5001";
 
-let token = localStorage.token;
-if (! token) {
-  localStorage.token = Math.random().toString(36).substr(-8);
-  token = localStorage.token;
-}
+const token = 'authtoken135';
 
 const headers = {
   'Accept': 'application/json',
