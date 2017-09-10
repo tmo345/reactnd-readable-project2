c59060300da44bf39b75c02eaf1ac2635c99acc8
Timothy Moore <moore.tim@outlook.com>
2017-09-10 13:10:34 -0400

    feat: Add Props type to AddPostButton component

--- src/components/AddPostButton.js
+++ src/components/AddPostButton.js
@@ -1,7 +1,14 @@
 // @flow
 import React from 'react';
+import type { AddPost } from '../actions/types';
+import type { AddPostData } from '../actions';
+import type { AddPostDispatch } from '../containers/Listings';
 
-export const AddPostButton = (props) => {
+type Props = {
+  addPost: AddPostDispatch
+}
+
+export const AddPostButton = (props: Props) => {
   return (<button
             onClick={() => props.addPost({title: 'things', author: 'yo', body: 'post', category: 'ayep'})}
           >New Post!</button>)
