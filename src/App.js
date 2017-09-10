7b17ea2513a9667e78ccbd45d34adc951cf1c2b9
Timothy Moore <moore.tim@outlook.com>
2017-09-10 10:38:08 -0400

    refactor: Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { addPost, setCategories } from './actions';
-import { connect } from 'react-redux';
-import type { ReduxStore } from './index.js';
-import type { AddPost, SetCategories, Action } from './actions/types';
-import type { AddPostData } from './actions';
-import type { Dispatch } from 'redux';
-import { getCategories } from './utils/api';
 import { PostDisplay } from './containers/Listings'
 
 
