d631d8f7393b2f1cb6b102ab030a090fcfcc91db
Timothy Moore <moore.tim@outlook.com>
2017-09-13 17:55:32 -0400

    refactor: Add cetegoryUI dispatches NavigationDisplay container component

--- src/containers/NavigationDisplay.js
+++ src/containers/NavigationDisplay.js
@@ -8,8 +8,14 @@ import type { Category } from 'store-types';
 import type { Action } from 'action-types';
 
 type Props = {
-  categories: Array<Category>,
-  setCategories: (categories: Array<Category>) => Dispatch<Action>
+  categories: Array<string>,
+  categoryUI: {
+    dropdownOpen: boolean,
+    active: string
+  },
+  setCategories: (categories: Array<Category>) => Dispatch<Action>,
+  toggleCategorySelect: () => Dispatch<Action>,
+  setActiveCategory: (category: string) => Dispatch<Action>
 }
 
 class NavigationDisplay extends Component<Props> {
@@ -23,7 +29,12 @@ class NavigationDisplay extends Component<Props> {
 
   render() {
     return (
-      <NavBar categories={this.props.categories} />
+      <NavBar
+        categories={this.props.categories}
+        categoryUI={this.props.categoryUI}
+        toggleCategorySelect={this.props.toggleCategorySelect}
+        setActiveCategory={this.props.setActiveCategory}
+       />
     )
   }
 }
@@ -33,8 +44,13 @@ const mapStateToProps = ({ categories, categoryUI }) => ({
   categoryUI
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<Action>): Dispatch<Action> => ({
-  setCategories: (categories: Array<Category>): Dispatch<Action> => dispatch(setCategories(categories))
-});
+const mapDispatchToProps = (dispatch: Dispatch<Action>): Dispatch<Action> =>  {
+
+  return {
+  setCategories: (categories: Array<Category>): Dispatch<Action> => dispatch(setCategories(categories)),
+  toggleCategorySelect: () => dispatch(toggleCategorySelect()),
+  setActiveCategory: (category: string) => dispatch(setActiveCategory(category))
+};
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavigationDisplay);
