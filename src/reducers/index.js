d20e73bb03dffbaf04f19cc7def315d5a7f65555
Timothy Moore <moore.tim@outlook.com>
2017-09-14 11:35:52 -0400

    feat: Define SET_ACTIVE_CATEGORY case in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -46,7 +46,22 @@ const initialCategories = [
 
 export const categories = (state: CategoryState = initialCategories, action: SetActiveCategory) => {
   switch(action.type) {
+    case 'SET_ACTIVE_CATEGORY':
+      const currentActive = (state.filter((category) => category.active))[0]
+      const targetCategory: CategoryName = action.name;
+      if (currentActive.name === targetCategory) {
+        return state;
       }
+      return (state.map((category) => {
+        if (category.name === action.name || category.active) {
+          return { ...category,
+            active: !category.active
+          }
+        } else {
+          return category
+        }
+      }))
+
     default:
       return state
   }
