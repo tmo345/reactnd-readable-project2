3deae8be7982b66e68f4af47603fc2b293260bb0
Timothy Moore <moore.tim@outlook.com>
2017-09-15 18:13:24 -0400

    styling: Style and layout single post view

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import { formatTime } from '../utils/helpers';
 import type { Post as PostType } from 'store-types';
 import type { EditPostDispatch } from '../containers/PostDisplay';
+import { Item, Grid, Header, Segment, Button, Icon } from 'semantic-ui-react';
+import CommentList from './CommentList';
 
 type Props = {
   post: PostType,
@@ -27,22 +29,50 @@ const Author = styled.p`
 `
 
 const CreatedAt = styled.p`
+`
 
+const ClearFloat = styled.p`
+  clear: both;
 `
 
 export const Post = (props: Props) => {
+  console.log('props', props)
   const id = props.post.id;
   const post = props.post;
+
   return (
-    <PostListItem
+    <div>
+    <Item
       onClick={() => props.editPost({ id: id, title: 'new title', body: 'new body'})}
     >
-      <Title>{post.title}</Title>
-      <Author>{post.author}</Author>
-      <CreatedAt>
-        {formatTime(post.timestamp)}
-      </CreatedAt>
-      <Body>{post.body}</Body>
-    </PostListItem>
-  )
+      <div>
+        <Segment color='blue' inverted attached='top'>
+          <Grid columns={2}>
+            <Grid.Column largeScreen={12}>
+              <Header color='black' inverted as="h2">{post.title}</Header>
+            </Grid.Column>
+            <Grid.Column largeScreen={4}>
+              <Header color='black' inverted as="h3" floated='right'>Votes: {post.voteScore}</Header>
+            </Grid.Column>
+          </Grid>
+        </Segment>
+        <Segment attached>
+          <Author>Author: {post.author}</Author>
+          <CreatedAt>
+            {formatTime(post.timestamp)}
+          </CreatedAt>
+          <Body>{post.body}</Body>
+          <Button floated='right' secondary>Delete</Button>
+          <Button floated='right' primary>Edit</Button>
+          <ClearFloat></ClearFloat>
+        </Segment>
+        <Segment attached='bottom'>
+          <p>Number of comments: 5</p>
+          <p>Vote: <Icon name='plus square outline' /><Icon name='minus square outline' /></p>
+        </Segment>
+      </div>
+    </Item>
+      <CommentList />
+    </div>
+    )
 }
