80f158b4d336f4ad5f2ad5406d5348798d1bc613
Timothy Moore <moore.tim@outlook.com>
2017-09-15 18:09:38 -0400

    refactor: Move Routes back to App

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
-import { MainView } from './components/MainView';
 import { PostDisplay } from './containers/PostDisplay';
 import NavigationDisplay from './containers/NavigationDisplay';
 import { Container, Grid,  } from 'semantic-ui-react';
-import SelectCategoryDisplay from './containers/SelectCategoryDisplay';
+import PostListDisplay from './containers/PostListDisplay';
 
 
 export default class App extends Component<*> {
@@ -18,18 +17,21 @@ export default class App extends Component<*> {
           <NavigationDisplay />
         </Grid.Row>
         <Container>
-          <Grid columns={2}>
-            <Grid.Row>
-              <Grid.Column largeScreen={10}>
-              <MainView />
-            </Grid.Column>
-            <Grid.Column largeScreen={6}>
-              <SelectCategoryDisplay />
-            </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
-      </Grid>
-      );
-  }
-}
+          <Route exact path='/:category?' component={PostListDisplay} />
+
+          <Route exact path='/:category/:id' render={(props) => (
+
+            <Grid columns={2}>
+              <Grid.Row>
+                <Grid.Column largeScreen={16}>
+                  <PostDisplay {...props} />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            )}>
+          </Route>
+          </Container>
+        </Grid>
+        );
+        }
+        }
