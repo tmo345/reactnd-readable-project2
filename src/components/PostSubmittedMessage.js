8522b225be776e274a88eb2c6a847042e4115566
Timothy Moore <moore.tim@outlook.com>
2017-10-03 12:16:08 -0400

    refactor: Rename button to accurately describe what it does

--- src/components/PostSubmittedMessage.js
+++ src/components/PostSubmittedMessage.js
@@ -4,7 +4,7 @@ import { Button } from 'semantic-ui-react';
 const PostSubmittedMessage = props => (
   <div>
     <h1>Post Submitted</h1>
-    <Button onClick={props.closeAddPostModal}>Return to all posts</Button>
+    <Button onClick={props.closeAddPostModal}>Close Modal</Button>
     <Button onClick={props.resetAddPostFormSubmitted}>Add new post</Button>
   </div>
 );
