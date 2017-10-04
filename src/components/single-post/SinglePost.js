7427ec2109e592e8dd2895ec8ba05d41158b863c
Timothy Moore <moore.tim@outlook.com>
2017-10-04 07:24:54 -0400

    style: Change styles and layout

--- src/components/single-post/SinglePost.js
+++ src/components/single-post/SinglePost.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import { formatTime } from '../../utils/helpers';
-import { Item, Grid, Header, Segment, Button, Loader } from 'semantic-ui-react';
-import CommentList from '../comment-list/CommentList';
-import VoteButtons from '../voting/VoteButtons';
-import SinglePostHeader from './SinglePostHeader';
+import { Item, Segment, Button, Loader } from 'semantic-ui-react';
 
 const Body = styled.div`margin-top: 0.5rem;`;
 const Author = styled.div``;
 const CreatedAt = styled.div``;
+const CommentNumber = styled.p`float: left;`;
 const ClearFloat = styled.div`clear: both;`;
-const InlineVotes = styled.h3`
-  display: inline-block;
-  margin-bottom: 0;
-`;
-const InlineVoteButtons = styled.div`
-  display: inline-block;
-  margin-left: 10px;
-`;
 
 const SinglePost = props => {
   const post = props.posts[props.urlId];
@@ -32,20 +22,19 @@ const SinglePost = props => {
               {post ? formatTime(post.timestamp) : renderLoader}
             </CreatedAt>
             <Body>{post ? post.body : renderLoader}</Body>
+          </Segment>
+          <Segment attached="bottom">
+            <CommentNumber>Number of comments: 5</CommentNumber>
             <Button floated="right" secondary>
               Delete
             </Button>
             <Button floated="right" primary>
               Edit
-            </Button>
+            </Button>{' '}
             <ClearFloat />
-          </Segment>
-          <Segment attached="bottom">
-            <p>Number of comments: 5</p>
           </Segment>
         </div>
       </Item>
-      <CommentList />
     </div>
   );
 };
