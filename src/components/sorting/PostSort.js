e2e8a6b7cd5eeb0a88b7c5c07234bd1a83dd5f62
Timothy Moore <moore.tim@outlook.com>
2017-10-06 23:02:37 -0400

    refactor: Reduce code repetition in the PostSort component

--- src/components/sorting/PostSort.js
+++ src/components/sorting/PostSort.js
@@ -1,42 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { List, Icon } from 'semantic-ui-react';
 
-const PostSort = props => (
-  <List horizontal>
-    <List.Item>
-      <List.Header>Sort By:</List.Header>
-      <List horizontal>
-        <List.Item
-          disabled={props.postsLoading}
-          onClick={() => props.setSortPostByFlag('timestamp', 'ascending')}
-          as="a"
-        >
-          Date Ascending
-        </List.Item>
-        <List.Item
-          disabled={props.postsLoading}
-          onClick={() => props.setSortPostByFlag('timestamp', 'descending')}
-          as="a"
-        >
-          Date Descending<Icon name="sort descending" />{' '}
-        </List.Item>
-        <List.Item
-          disabled={props.postsLoading}
-          onClick={() => props.setSortPostByFlag('voteScore', 'ascending')}
-          as="a"
-        >
-          Votes Ascending
-        </List.Item>
-        <List.Item
-          disabled={props.postsLoading}
-          onClick={() => props.setSortPostByFlag('voteScore', 'descending')}
-          as="a"
-        >
-          Votes Descending
-        </List.Item>
-      </List>
-    </List.Item>
-  </List>
-);
+class PostSort extends Component {
+  state = {
+    activeSortValue: 'timestamp-descending',
+  };
+
+  sortOrders = [
+    {
+      value: 'timestamp-descending',
+      text: 'Newest Posts First',
+    },
+    {
+      value: 'timestamp-ascending',
+      text: 'Oldest Posts First',
+    },
+    {
+      value: 'voteScore-descending',
+      text: 'Highest Votes First',
+    },
+    {
+      value: 'voteScore-ascending',
+      text: 'Lowest Votes First',
+    },
+  ];
+
+  setActiveSort = sort => this.setState({ activeSortValue: sort });
+
+  handleSetPostFlag = e => {
+    const order = e.target.value;
+    this.setActiveSort(order);
+
+    const flag = order.split('-')[0];
+    const direction = order.split('-')[1];
+    this.props.setSortPostByFlag(flag, direction);
+  };
+
+  render() {
+    return (
+      <select
+        onChange={this.handleSetPostFlag}
+        value={this.state.activeSortValue}
+      >
+        {this.sortOrders.map(order => (
+          <option key={order.value} value={order.value}>
+            {order.text}
+          </option>
+        ))}
+      </select>
+    );
+  }
+}
 
 export default PostSort;
