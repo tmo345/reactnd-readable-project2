c0a66877e596321e384897a6c2ed5a6ccb1e32d1
Timothy Moore <moore.tim@outlook.com>
2017-10-14 00:26:11 -0400

    refactor: Delete unused functions and use implicit return in arrow functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,34 +9,15 @@ const headers = {
   Authorization: token,
 };
 
-export const fetchFromApi = () => {
-  return fetch(`${api}/categories`, { headers });
-};
-
 export const fetchCategories: fetch = () =>
   fetch(`${api}/categories`, { headers })
     .then(res => res.json())
-    .then(data => {
-      return data.categories;
-    });
+    .then(data => data.categories);
 
 export const fetchPosts = () =>
   fetch(`${api}/posts`, { headers })
-    .then(res => res.json(), error => console.log('An error occurs', error))
-    .then(data => {
-      return data;
-    });
-
-export const fetchPost = id =>
-  fetch(`${api}/posts/${id}`, { headers })
-    .then(res => {
-      console.log('got response from server');
-      return res.json();
-    })
-    .then(data => {
-      console.log('data', data);
-      return data;
-    });
+    .then(res => res.json())
+    .then(data => data);
 
 export const fetchPostsByCategory = category =>
   fetch(`${api}/${category}/posts`, { headers })
